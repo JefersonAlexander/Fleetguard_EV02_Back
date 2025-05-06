dc7eb0ff612149a62986d378c1de76dd43106367
Jeferson Alexander Del Rio Herrera <yeferdrh@gmail.com>
2025-05-05 22:05:06 -0500

    Initial commit of FleetGuard360 project

---
+++ microservice-fleetLocation/src/test/java/com/microservice/fleetLocation/MicroserviceFleetLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.fleetLocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceFleetLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
